9c15dd77661ce41172eaee996c5ce4c72d5ecdc2
Sebastian Larsson <sjereq@gmail.com>
2014-10-17 11:00:19 +0200

    Implement gcd in assembly

--- lexyacc-code/prologue.s
+++ lexyacc-code/prologue.s
@@ -1,3 +1,27 @@
+
+.section .text
+gcd:
+	pushl	%ebp
+	movl	%esp, %ebp
+
+	movl	8(%ebp), %ecx
+	movl	12(%ebp), %eax
+	
+1:
+	cmpl	%eax, %ecx
+	je	1f
+	cmpl	%ecx, %eax
+	jle	2f
+	subl	%ecx, %eax
+	jmp	1b
+2:
+	subl	%eax, %ecx
+	jmp	1b
+
+1:
+	leave
+	ret
+
 .section .bss
 vars:
 	.space	26 * 4
